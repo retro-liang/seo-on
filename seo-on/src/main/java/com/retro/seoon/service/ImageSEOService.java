b3abdf19c5de021b98962c14cc3cf7c7bd686355
Liang Zhao <retro.liang@gmail.com>
2012-06-13 16:43:06 -0400

    w

--- seo-on/src/main/java/com/retro/seoon/service/ImageSEOService.java
+++ seo-on/src/main/java/com/retro/seoon/service/ImageSEOService.java
@@ -1,14 +1,30 @@
 package com.retro.seoon.service;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.files.AppEngineFile;
+import com.google.appengine.api.files.FileReadChannel;
+import com.google.appengine.api.files.FileService;
+import com.google.appengine.api.files.FileServiceFactory;
+import com.google.appengine.api.files.FileWriteChannel;
 import com.google.appengine.api.images.Composite;
+import com.google.appengine.api.images.Composite.Anchor;
 import com.google.appengine.api.images.Image;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesService.OutputEncoding;
@@ -19,32 +35,19 @@ public class ImageSEOService
 {
 	private static final Logger logger = Logger.getLogger(ImageSEOService.class.getName());
 	
-	public static void seoImage()
+	private ImagesService imagesService = ImagesServiceFactory.getImagesService();
+	private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+	
+	public BlobKey seoImage(HttpServletRequest request, byte[] fileContent) throws IOException
 	{
-		File file1 = new File("favicon.ico");
-		File file2 = new File("Google-G-Logo.png");
-		
-		byte[] oldImageData1 = null;
-		byte[] oldImageData2 = null;
-		try
-		{
-			oldImageData1 = getBytesFromFile(file1);
-			oldImageData2 = getBytesFromFile(file2);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		
-		
+        Anchor anchor = Composite.Anchor.TOP_LEFT;
 
-        ImagesService imagesService = ImagesServiceFactory.getImagesService();
-
-        Image oldImage1 = ImagesServiceFactory.makeImage(oldImageData1);
-        Composite composit1 = ImagesServiceFactory.makeComposite(oldImage1, 0, 0, 1, null);
+        Image oldImage1 = ImagesServiceFactory.makeImage(fileContent);
+        Composite composit1 = ImagesServiceFactory.makeComposite(oldImage1, 0, 0, 1, anchor);
         
-        Image oldImage2 = ImagesServiceFactory.makeImage(oldImageData2);
-        Composite composit2 = ImagesServiceFactory.makeComposite(oldImage2, 0, 0, 1, null);
+        Image oldImage2 = ImagesServiceFactory.makeImage(fileContent);
+        Composite composit2 = ImagesServiceFactory.makeComposite(oldImage2, 0, 0, 1, anchor);
         //Transform resize = ImagesServiceFactory.makeResize(200, 300);
 
         //Image newImage = imagesService.applyTransform(resize, oldImage);
@@ -58,6 +61,20 @@ public class ImageSEOService
 		OutputSettings outputSettings = new OutputSettings(outputEncoding);
 		Image newImage = imagesService.composite(composites, oldImage1.getWidth(), 1000, 0l, outputSettings);
 		logger.info("new image:" +newImage);
+		
+		return saveFile(newImage);
+		
+		/*Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+		List<BlobKey> blobKey = blobs.get("file");
+
+		if (blobKey == null)
+		{
+		    return "/";
+		}
+		else
+		{
+		    return "/image?blob-key=" + blobKey.get(0).getKeyString();
+		}*/
 		/*File path = ""; // base path of the images
 
 		// load source images
@@ -76,6 +93,61 @@ public class ImageSEOService
 
 		// Save as new image
 		ImageIO.write(combined, "PNG", new File(path, "combined.png"));*/
+	}
+	
+	public BlobKey saveFile(Image image) throws IOException
+	{
+		// Get a file service
+		  FileService fileService = FileServiceFactory.getFileService();
+
+		  // Create a new Blob file with mime-type "text/plain"
+		  AppEngineFile file = fileService.createNewBlobFile("multipart/form-data");
+
+		  // Open a channel to write to it
+		  boolean lock = false;
+		  FileWriteChannel writeChannel = fileService.openWriteChannel(file, lock);
+
+		  // Different standard Java ways of writing to the channel
+		  // are possible. Here we use a PrintWriter:
+		  PrintWriter out = new PrintWriter(Channels.newWriter(writeChannel, "UTF8"));
+		  out.println(image.getImageData());		  
+
+		  // Close without finalizing and save the file path for writing later
+		  out.close();
+		  String path = file.getFullPath();
+
+		  // Write more to the file in a separate request:
+		  file = new AppEngineFile(path);
+
+		  // This time lock because we intend to finalize
+		  lock = true;
+		  writeChannel = fileService.openWriteChannel(file, lock);
+
+		  // This time we write to the channel directly
+		  //writeChannel.write(ByteBuffer.wrap("And miles to go before I sleep.".getBytes()));
+
+		  // Now finalize
+		  writeChannel.closeFinally();
+
+		  // Later, read from the file using the file API
+		  lock = false; // Let other people read at the same time
+		  FileReadChannel readChannel = fileService.openReadChannel(file, false);
+
+		  // Again, different standard Java ways of reading from the channel.
+		  BufferedReader reader = new BufferedReader(Channels.newReader(readChannel, "UTF8"));
+		  String line = reader.readLine();
+		  logger.info("Line: " + line);
+		  // line = "The woods are lovely dark and deep."
+
+		  readChannel.close();
+
+		  // Now read from the file using the Blobstore API
+		  BlobKey blobKey = fileService.getBlobKey(file);
+		  logger.info("Saved key: " + blobKey.getKeyString());
+		  
+		  return blobKey;
+		  //BlobstoreService blobStoreService = BlobstoreServiceFactory.getBlobstoreService();
+		  //String segment = new String(blobStoreService.fetchData(blobKey, 30, 40));
 	}
 	
 	public static byte[] getBytesFromFile(File file) throws IOException
@@ -112,5 +184,16 @@ public class ImageSEOService
 	    // Close the input stream and return bytes
 	    is.close();
 	    return bytes;
+	}
+	
+	public static void main(String args[])
+	{
+		File file = new File("D:\\workspace_git\\seo-on\\seo-on\\src\\main\\resources\\favicon.ico");
+		try {
+			System.out.println(getBytesFromFile(file));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- seo-on/src/main/java/com/retro/seoon/web/SeoOnController.java
+++ seo-on/src/main/java/com/retro/seoon/web/SeoOnController.java
@@ -1,26 +1,75 @@
 package com.retro.seoon.web;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.images.Image;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
 import com.retro.seoon.service.ImageSEOService;
 
 @Controller
 @RequestMapping("/spring")
 public class SeoOnController
 {
+	private static final Logger logger = Logger.getLogger(SeoOnController.class.getName());
+	
+	private @Autowired HttpServletRequest request;	
+
 	@RequestMapping(method = RequestMethod.GET)
     public String welcomeHandler()
 	{
-		ImageSEOService.seoImage();
+		logger.info("request: " + request.getAttributeNames());
 		return "welcome";
     }
 	
-	@RequestMapping("image")
-    public String handler()
+	@RequestMapping(method = RequestMethod.POST)
+    public void welcomePOSTHandler(@RequestParam("file") MultipartFile[] imageFiles, HttpServletResponse response)
 	{
-		ImageSEOService.seoImage();
-		return "";
+		logger.info("f: " + imageFiles);
+		try {
+			BlobKey key =  new ImageSEOService().seoImage(request, imageFiles[0].getBytes());
+			ImagesService imagesService = ImagesServiceFactory.getImagesService();
+			String url = imagesService.getServingUrl(key);
+			logger.info("url: " + url);
+			BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+			blobstoreService.serve(key, response);
+			logger.info("image served: " + key);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//return "welcome";
+    }
+	
+	@RequestMapping("image")
+    public String handler(@RequestParam("blob-key") BlobKey key, HttpServletResponse response)
+	{
+		logger.info("key: " + key);
+		/*BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();		
+        try {
+			blobstoreService.serve(key, response);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+        
+        ImagesService imagesService = ImagesServiceFactory.getImagesService();
+        String url = imagesService.getServingUrl(key);
+        logger.info("url: " + url);
+		return url;
     }
 }
